c8a1516d623e292ce89da93da80e9df51189b8af
Xewol <bartekdeluga0702@gmail.com>
2023-11-24 01:37:06 +0100

    feat: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,85 @@
-import Image from 'next/image'
+import {
+  faNoteSticky,
+  faChartSimple,
+  faMountainCity,
+  faTornado,
+  faPuzzlePiece,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="w-full p-12">
+      <h1 className="text-6xl font-bold">
+        Welcome to<span className="text-orange10"> Questlog</span>
+      </h1>
+      <p className="text-2xl font-bold text-mauve11 mt-2">
+        Empower Your Team, Complete Quests!
+      </p>
+      <div className="flex mt-10 text-mauve max-w-xl">
+        <ul className="grid gap-4">
+          <li>
+            <div className="border p-4 rounded border-mauve4 flex flex-col h-full">
+              <p className="font-bold text-xl">
+                Management with a Twist <FontAwesomeIcon icon={faTornado} />
+              </p>
+              <p className="text-mauve11">
+                Effortlessly create, edit, and delete projects. Assign tasks,
+                set deadlines, and prioritize with ease.
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="border p-4 rounded border-mauve4 flex flex-col h-full">
+              <p className="font-bold text-xl">
+                Task Management Made Fun{" "}
+                <FontAwesomeIcon icon={faPuzzlePiece} />
+              </p>
+              <p className="text-mauve11">
+                Create, edit, and delete tasks within projects. Assign tasks to
+                team members, set due dates, and track progress.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="border p-4 rounded border-mauve4 flex flex-col h-full">
+              <p className="font-bold text-xl">
+                City Building Element <FontAwesomeIcon icon={faMountainCity} />
+              </p>
+              <p className="text-mauve11">
+                Witness the growth of your city as your team accomplishes tasks.
+                Each completed task contributes to the development and expansion
+                of your virtual city.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="border p-4 rounded border-mauve4 flex flex-col h-full">
+              <p className="font-bold text-xl">
+                Progress Tracking <FontAwesomeIcon icon={faChartSimple} />
+              </p>
+              <p className="text-mauve11">
+                Stay informed with real-time statistics, charts, and reports.
+                Track your team&apos;s achievements and project milestones
+                effortlessly.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="border p-4 rounded border-mauve4 flex flex-col h-full">
+              <p className="font-bold text-xl">
+                Notifications and Reminders{" "}
+                <FontAwesomeIcon icon={faNoteSticky} />
+              </p>
+              <p className="text-mauve11">
+                Never miss a deadline again! TaskCity keeps you in the loop with
+                timely notifications and reminders.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
