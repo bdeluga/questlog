6bd6052cc86ba32feb2e5e650aa3dc47177d7773
Xewol <bartekdeluga0702@gmail.com>
2023-11-26 15:51:27 +0100

    feat: t3oss env

---
+++ env.mjs
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    GITHUB_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    NEXTAUTH_SECRET: z.string(),
+  },
+
+  experimental__runtimeEnv: {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+});
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import "./env.mjs";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: '"avatars.githubusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
