7057e543f30d52c9d6d82c47227037e80d64885a
Xewol <bartekdeluga0702@gmail.com>
2023-11-28 23:22:51 +0100

    refactor: restructured

--- app/(mainflow)/layout.tsx
+++ app/(mainflow)/layout.tsx
@@ -2,31 +2,75 @@ import Link from "next/link";
 import React from "react";
 import UserBadge from "./components/UserBadge";
 import { auth } from "~/app/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookJournalWhills,
+  faCaretDown,
+  faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 async function MainFlowLayout({ children }: { children: React.ReactElement }) {
   const user = await auth();
+
+  const selectedProject = false || "Project #1";
+
   return (
     <>
-      <header className="flex justify-end p-4 border-b border-mauve3">
+      <header className="flex justify-between items-center p-4 ">
         {user?.user ? (
-          <UserBadge user={user.user} />
+          <>
+            <nav className="flex items-center gap-3 h-full ">
+              <Link href={"/"} className="text-4xl">
+                <FontAwesomeIcon
+                  icon={faBookJournalWhills}
+                  className="text-orange11"
+                />
+              </Link>
+
+              <div className="w-0.5 rounded-md py-4 bg-mauve3 rotate-12" />
+              <div className="flex items-center gap-1 text-xl">
+                <div>{selectedProject}</div>
+                <button className="flex flex-col hover:bg-mauve3 p-1 px-2 text-sm rounded-md -space-y-1">
+                  <FontAwesomeIcon icon={faCaretUp} />
+                  <FontAwesomeIcon icon={faCaretDown} />
+                </button>
+              </div>
+            </nav>
+            <UserBadge user={user.user} />
+          </>
         ) : (
-          <div className="flex gap-4">
-            <Link
-              className="rounded-lg flex justify-center items-center text-center px-3 py-1.5  border border-mauve4 bg-mauve2 hover:border-mauve5 hover:bg-mauve3"
-              href={"/sign-in"}
-            >
-              Sign in
+          <>
+            <Link href={"/"} className="text-4xl">
+              <FontAwesomeIcon
+                icon={faBookJournalWhills}
+                className="text-orange11"
+              />
             </Link>
-            <Link
-              className="rounded-lg flex justify-center items-center text-center px-4 py-1.5  border border-mauve11 hover:bg-mauve12 hover:text-mauve1"
-              href={"/sign-up"}
-            >
-              Sign up
-            </Link>
-          </div>
+            <div className="flex gap-4">
+              <Link
+                className="rounded-lg flex justify-center items-center text-center px-3 py-1.5  border border-mauve4 bg-mauve2 hover:border-mauve5 hover:bg-mauve3"
+                href={"/sign-in"}
+              >
+                Sign in
+              </Link>
+              <Link
+                className="rounded-lg flex justify-center items-center text-center px-4 py-1.5  border border-mauve11 hover:bg-mauve12 hover:text-mauve1"
+                href={"/sign-up"}
+              >
+                Sign up
+              </Link>
+            </div>
+          </>
         )}
       </header>
+      <div className=" border-mauve3 border-b p-4">
+        <nav className="space-x-6">
+          <Link href={"/dashboard"}>Overview</Link>
+          <Link href={"/dashboard/board"}>Board</Link>
+          <Link href={"/dashboard/settings"}>Settings</Link>
+          <Link href={"/dashboard/integrations"}>Integrations</Link>
+        </nav>
+      </div>
       {children}
     </>
   );
