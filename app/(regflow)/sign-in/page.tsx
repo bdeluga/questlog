5ead40901cadc6904d4748e0697456d62ee640e5
Xewol <bartekdeluga0702@gmail.com>
2023-12-01 18:46:11 +0100

    feat: hover github button

--- app/(regflow)/sign-in/page.tsx
+++ app/(regflow)/sign-in/page.tsx
@@ -4,7 +4,6 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 function SignIn() {
@@ -17,7 +16,7 @@ function SignIn() {
         <li>
           <button
             onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
-            className="border px-4 py-2 text-2xl bg-mauve3 border-mauve4 rounded-md"
+            className="border px-4 py-2 text-2xl bg-mauve3 border-mauve4 hover:bg-mauve4 hover:border-mauve5 rounded-md"
           >
             <FontAwesomeIcon icon={faGithub} /> Continue with GitHub
           </button>
