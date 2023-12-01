b495a5eb3594e49f30d42705206d2661bdd9a282
Xewol <bartekdeluga0702@gmail.com>
2023-12-01 18:44:53 +0100

    feat: env updated

--- env.mjs
+++ env.mjs
@@ -6,11 +6,13 @@ export const env = createEnv({
     GITHUB_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
     NEXTAUTH_SECRET: z.string(),
+    JWT_SECRET: z.string(),
   },
 
   experimental__runtimeEnv: {
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
 });
