66975f4ed7f838a70977e5daaa9f61f84cf9ef37
Xewol <bartekdeluga0702@gmail.com>
2023-12-02 05:10:47 +0100

    feat: header

--- app/(mainflow)/layout.tsx
+++ app/(mainflow)/layout.tsx
@@ -16,53 +16,50 @@ async function MainFlowLayout({ children }: { children: React.ReactElement }) {
 
   return (
     <>
-      <header className="flex justify-between items-center p-4 border-mauve3 border-b">
-        {user?.user ? (
-          <>
-            <nav className="flex items-center gap-3 h-full ">
-              <Link href={"/dashboard"} className="text-4xl">
-                <FontAwesomeIcon
-                  icon={faBookJournalWhills}
-                  className="text-orange11"
-                />
-              </Link>
-
-              <div className="w-0.5 rounded-md py-4 bg-mauve3 rotate-12" />
-              <div className="flex items-center gap-1 text-xl">
-                <div>{selectedProject}</div>
-                <button className="flex flex-col hover:bg-mauve3 p-1 px-2 text-sm rounded-md -space-y-1">
-                  <FontAwesomeIcon icon={faCaretUp} />
-                  <FontAwesomeIcon icon={faCaretDown} />
-                </button>
-              </div>
-            </nav>
-            <UserBadge user={user.user} />
-          </>
-        ) : (
-          <>
-            <Link href={"/"} className="text-4xl">
+      {user?.user ? (
+        <header className="flex justify-between items-center p-4 border-mauve3 ">
+          <nav className="flex items-center gap-3 h-full ">
+            <Link href={"/dashboard"} className="text-4xl">
               <FontAwesomeIcon
                 icon={faBookJournalWhills}
                 className="text-orange11"
               />
             </Link>
-            <div className="flex gap-4">
-              <Link
-                className="rounded-lg flex justify-center items-center text-center px-3 py-1.5  border border-mauve4 bg-mauve2 hover:border-mauve5 hover:bg-mauve3"
-                href={"/sign-in"}
-              >
-                Sign in
-              </Link>
-              <Link
-                className="rounded-lg flex justify-center items-center text-center px-4 py-1.5  border border-mauve11 hover:bg-mauve12 hover:text-mauve1"
-                href={"/sign-up"}
-              >
-                Sign up
-              </Link>
+            <div className="w-0.5 rounded-md py-4 bg-mauve3 rotate-12" />
+            <div className="flex items-center gap-1 text-xl">
+              <div>{selectedProject}</div>
+              <button className="flex flex-col hover:bg-mauve3 p-1 px-2 text-sm rounded-md -space-y-1">
+                <FontAwesomeIcon icon={faCaretUp} />
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
             </div>
-          </>
-        )}
-      </header>
+          </nav>
+          <UserBadge user={user.user} />
+        </header>
+      ) : (
+        <header className="flex justify-between items-center p-4 border-mauve3 border-b">
+          <Link href={"/"} className="text-4xl">
+            <FontAwesomeIcon
+              icon={faBookJournalWhills}
+              className="text-orange11"
+            />
+          </Link>
+          <div className="flex gap-4">
+            <Link
+              className="rounded-lg flex justify-center items-center text-center px-3 py-1.5  border border-mauve4 bg-mauve2 hover:border-mauve5 hover:bg-mauve3"
+              href={"/sign-in"}
+            >
+              Sign in
+            </Link>
+            <Link
+              className="rounded-lg flex justify-center items-center text-center px-4 py-1.5  border border-mauve11 hover:bg-mauve12 hover:text-mauve1"
+              href={"/sign-up"}
+            >
+              Sign up
+            </Link>
+          </div>
+        </header>
+      )}
       {children}
     </>
   );
