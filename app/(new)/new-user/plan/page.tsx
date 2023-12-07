7da96dd48332d5b523096dcafdc38093568f5925
Xewol <bartekdeluga0702@gmail.com>
2023-12-07 13:04:34 +0100

    feat: new user exception

--- app/(new)/new-user/plan/page.tsx
+++ app/(new)/new-user/plan/page.tsx
@@ -4,15 +4,13 @@ import PlanForm from "@/app/components/forms/PlanForm";
 import React from "react";
 
 export default async function PlanPage() {
-  const user = await auth();
-
   return (
     <div className="min-h-screen flex flex-col font-bold justify-center items-center">
       <h1 className="text-4xl mb-4 text-center">
         Choose a <span className="text-orange10">Questlog</span>
         <br /> account plan
       </h1>
-      <PlanForm user={user?.user} />
+      <PlanForm />
     </div>
   );
 }
--- app/components/forms/PlanForm.tsx
+++ app/components/forms/PlanForm.tsx
@@ -1,21 +1,38 @@
 "use client";
+import { useToastStore } from "@/app/store/ToastStore";
 import Checkbox from "@/ui/Checkbox";
 import getFormData from "@/utils/getFormData";
-import { Session } from "next-auth";
+import { redirect, useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 
-export default function PlanForm({ user }: { user: Session["user"] }) {
+export default function PlanForm() {
+  const notify = useToastStore((slice) => slice.notify);
+
   const [checked, setChecked] = useState(false);
+
+  const router = useRouter();
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = getFormData(event.currentTarget);
-    alert(`set plan for user:${user?.name}`);
+
+    await fetch("/api/plan", {
+      method: "PATCH",
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        router.push("/dashboard");
+        notify({
+          title: "Success",
+          description: "Account plan was succesfully updated.",
+        });
+      }
+    });
   };
 
   return (
     <form className="max-w-sm w-full" onSubmit={handleSubmit}>
-      <fieldset className="">
+      <fieldset>
         <label className="text-lg text-mauve10">Plan type</label>
         <ul className="space-y-4 mt-1">
           <li className="border-2 border-mauve4 rounded-md p-2  flex items-center">
@@ -27,7 +44,9 @@ export default function PlanForm({ user }: { user: Session["user"] }) {
             </div>
             <Checkbox
               checked={checked}
-              onCheckedChange={setChecked}
+              name="plan"
+              value="hobby"
+              onCheckedChange={() => setChecked(!checked)}
               className=" border-2 text-mauve1 data-[state=checked]:bg-orange10 data-[state=checked]:border-orange10 border-mauve6 rounded-full h-6 w-6 flex justify-center items-center mr-2"
             />
           </li>
@@ -41,6 +60,8 @@ export default function PlanForm({ user }: { user: Session["user"] }) {
       <button
         className=" w-full mt-12 p-4 text-center rounded bg-mauve12 text-mauve1 disabled:opacity-50"
         disabled={!checked}
+        name="plan"
+        value="pro"
       >
         Continue
       </button>
