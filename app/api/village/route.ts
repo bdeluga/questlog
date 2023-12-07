e6ddbbfbf55095a9d942b86032e3abcba3d96311
Xewol <bartekdeluga0702@gmail.com>
2023-12-07 13:05:31 +0100

    feat: routes

--- app/api/village/route.ts
+++ app/api/village/route.ts
@@ -1,11 +1,15 @@
+import { auth } from "@/app/auth";
 import { db } from "@/db";
-import { village } from "@/db/schema";
+import { villages } from "@/db/schema";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  await db.insert(village).values({
-    ...data,
-  });
+  const user = await auth();
+  console.log(user);
 
-  return NextResponse.json({ status: 200 });
+  // const data = await req.json();
+  // await db.insert(villages).values({
+  //   ...data,
+  // });
+
+  return NextResponse.json({ status: 400 });
 }
