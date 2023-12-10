f605cef1a1f590150b394f56f60e37e78ac11c62
Xewol <bartekdeluga0702@gmail.com>
2023-12-10 01:38:26 +0100

    changed: items alignment

--- app/components/OverviewPanels/Level.tsx
+++ app/components/OverviewPanels/Level.tsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Level() {
   return (
     <div className="flex flex-col h-full justify-between">
-      <div className="flex justify-between items-center text-lg">
+      <div className="flex justify-between items-baseline text-lg">
         Level
         <div className="flex items-center gap-1 bg-mauve3 w-fit px-2  rounded-md">
           <FontAwesomeIcon icon={faFire} className="text-orange11" />
