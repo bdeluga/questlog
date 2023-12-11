eff0c6800590e8423805c032ff1a21d74d86ca56
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-11 12:22:40 +0100

    feat: toast animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,23 @@ module.exports = {
           from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
           to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
         },
+        hide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        slideIn: {
+          from: {
+            transform: "translateX(calc(100% + 24px))",
+          },
+          to: { transform: "translateX(0)" },
+        },
       },
       animation: {
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        hide: "hide 100ms ease-in",
+        slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        swipeOut: "swipeOut 100ms ease-out",
       },
     },
   },
