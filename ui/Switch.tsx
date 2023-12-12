5eeaec6ccd887724c1beb398839dcdee4953a647
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-12 15:48:46 +0100

    update: removed unused relative

--- ui/Switch.tsx
+++ ui/Switch.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Switch({ id, defaultChecked }: Props) {
   return (
     <SwitchMenu.Root
-      className="w-10 h-6 bg-mauve2 rounded-full relative border border-mauve5 data-[state=checked]:bg-mauve4 data-[state=checked]:border-mauve6 outline-none cursor-default"
+      className="w-10 h-6 bg-mauve2 rounded-full border border-mauve5 data-[state=checked]:bg-mauve4 data-[state=checked]:border-mauve6 outline-none cursor-default"
       id={id}
       defaultChecked={defaultChecked}
     >
