fc2f33ea93cb0df135674a5d54cad367f52862b6
Xewol <bartekdeluga0702@gmail.com>
2023-12-12 22:33:59 +0100

    feat: notice board

---
+++ app/components/NoticeBoard.tsx
@@ -0,0 +1,108 @@
+import { TaskMap, reorderTasks } from "@/utils/reorder";
+import { useState } from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+  resetServerContext,
+} from "react-beautiful-dnd";
+
+export default function NoticeBoard({
+  tasks,
+  headers,
+}: {
+  tasks: TaskMap;
+  headers: string[];
+}) {
+  resetServerContext();
+
+  const [userTasks, setUserTasks] = useState(tasks);
+
+  const onDragEnd = (result: DropResult) => {
+    const source = result?.source;
+    const destination = result?.destination;
+
+    if (!destination) {
+      return;
+    }
+
+    // did not move anywhere - can bail early
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    const reorderedTasks = reorderTasks({
+      tasks: userTasks,
+      source,
+      destination,
+    });
+
+    setUserTasks(reorderedTasks);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="taskboard" type="COLUMN" direction="horizontal">
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="grid grid-cols-4 h-full divide-x"
+          >
+            {/* Render columns for each key in initialTasks */}
+            {headers.map((key) => {
+              return (
+                <div key={key} className="border-mauve4 flex-1 flex flex-col">
+                  <h2 className="text-xl text-center bg-mauve2 capitalize p-2 border-mauve4 border-y ">
+                    {key}
+                  </h2>
+                  <Droppable droppableId={key}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className={`flex-1 p-4  ${
+                          snapshot.isDraggingOver &&
+                          !snapshot.draggingFromThisWith
+                            ? "bg-mauve3/50"
+                            : ""
+                        }`}
+                      >
+                        {/* Render tasks within each column */}
+                        {userTasks[key as keyof typeof userTasks].map(
+                          (task, index) => (
+                            <Draggable
+                              key={task.id}
+                              draggableId={task.id}
+                              index={index}
+                            >
+                              {(provided) => (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  className="bg-white p-4 rounded mb-2 bg-mauve4"
+                                >
+                                  {task.content}
+                                </div>
+                              )}
+                            </Draggable>
+                          )
+                        )}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
