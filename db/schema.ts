6a06b3d73bdd4671f4d2376eafc526eea86af9a9
Xewol <bartekdeluga0702@gmail.com>
2023-12-13 22:01:07 +0100

    feat: unique name per user

--- db/schema.ts
+++ db/schema.ts
@@ -1,20 +1,20 @@
 import {
-  text,
-  date,
-  pgTable,
-  numeric,
-  PgEnum,
-  pgEnum,
-} from "drizzle-orm/pg-core";
-import {
   InferInsertModel,
   InferSelectModel,
   relations,
   sql,
 } from "drizzle-orm";
+import {
+  text,
+  date,
+  pgEnum,
+  pgTable,
+  uniqueIndex,
+  numeric,
+} from "drizzle-orm/pg-core";
 
-export const planEnum = pgEnum("plan", ["hobby", "pro"]);
-
+const planEnum = pgEnum("plan", ["hobby", "pro"]);
+const stateEnum = pgEnum("state", ["new", "active", "resolved", "closed"]);
 export const users = pgTable("users", {
   id: text("id")
     .default(sql`gen_random_uuid()`)
@@ -31,37 +31,45 @@ export const users = pgTable("users", {
 
 export const usersRelations = relations(users, ({ many }) => ({
   villages: many(villages),
+  quests: many(quests),
 }));
 
 export type User = InferSelectModel<typeof users>;
 export type NewUser = InferInsertModel<typeof users>;
 
-export const villages = pgTable("villages", {
-  id: text("id")
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  name: text("name").notNull(),
-  userId: text("user_id")
-    .references(() => users.id)
-    .notNull(),
-  exp: numeric("exp").notNull().default("1"),
-  expNeeded: numeric("exp_needed").notNull().default("150"),
-  level: numeric("level").default("1"),
-});
+export const villages = pgTable(
+  "villages",
+  {
+    id: text("id")
+      .default(sql`gen_random_uuid()`)
+      .primaryKey(),
+    name: text("name").notNull(),
+    userId: text("user_id")
+      .references(() => users.id)
+      .notNull(),
+    exp: numeric("exp").notNull().default("1"),
+    expNeeded: numeric("exp_needed").notNull().default("150"),
+    level: numeric("level").default("1"),
+  },
+  (table) => {
+    return {
+      userVillageIdx: uniqueIndex("user_village_idx").on(
+        table.userId,
+        table.name
+      ),
+    };
+  }
+);
 
-export const villagesRelations = relations(villages, ({ one }) => ({
-  user: one(users, { fields: [villages.userId], references: [users.id] }),
+export const villagesRelations = relations(villages, ({ one, many }) => ({
+  user: one(users, {
+    fields: [villages.userId],
+    references: [users.id],
+  }),
+  quests: many(quests),
 }));
-
 export type Village = InferSelectModel<typeof villages>;
 export type NewVillage = InferInsertModel<typeof villages>;
-
-export const stateEnum = pgEnum("state", [
-  "new",
-  "active",
-  "resolved",
-  "closed",
-]);
 
 export const quests = pgTable("quests", {
   id: text("id")
@@ -78,6 +86,11 @@ export const quests = pgTable("quests", {
   rewardExp: numeric("reward_exp").notNull(),
   state: stateEnum("state").default("new").notNull(),
 });
+
+export const questsRelations = relations(quests, ({ one }) => ({
+  village: one(villages),
+  user: one(users),
+}));
 
 export type Quest = InferSelectModel<typeof quests>;
 export type NewQuest = InferInsertModel<typeof quests>;
