cee90fa0067a994dc5ea1677a98c2bc05b894818
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-13 10:57:07 +0100

    fix: make vercel deploy

--- app/actions/addPlanAction.ts
+++ app/actions/addPlanAction.ts
@@ -1,6 +1,6 @@
 "use server";
 import { db } from "@/db";
-import { users } from "@/db/schema";
+import { User, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -8,11 +8,11 @@ export default async function addPlanAction(
   formData: FormData,
   userId: string
 ) {
-  const plan = formData.get("plan");
+  const plan = formData.get("plan") as User["plan"];
   await db
     .update(users)
     .set({
-      plan: plan as string,
+      plan: plan,
     })
     .where(eq(users.id, userId));
   revalidatePath("/");
