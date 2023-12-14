5515d473fb6a462fe15e9ceef99c00531f47e23b
Xewol <bartekdeluga0702@gmail.com>
2023-12-14 05:20:25 +0100

    fix: wrong calculations

--- ui/ProgressBar.tsx
+++ ui/ProgressBar.tsx
@@ -19,7 +19,11 @@ export default function ProgressBar({
       >
         <Progress.Indicator
           className="bg-orange11 group-hover:opacity-50 relative flex w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-          style={{ transform: `translateX(-${100 - progress}%)` }}
+          style={{
+            transform: `translateX(-${
+              100 - Math.floor((progress / progressNeeded) * 100)
+            }%)`,
+          }}
         />
         <div className="absolute opacity-0 group-hover:opacity-100 duration-150 text-orange12/50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
           {progress}/{progressNeeded}
