903902131c0501544114810048e645122bfca941
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-15 15:32:04 +0100

    refactor: changed logic

--- app/(authflow)/[village]/journal/page.tsx
+++ app/(authflow)/[village]/journal/page.tsx
@@ -1,50 +1,31 @@
-import NoticeBoard from "@/app/components/NoticeBoard";
+import NoticeBoard from "@/app/components/NoticeBoard/NoticeBoard";
+import NoticeBoardMenu from "@/app/components/NoticeBoard/NoticeBoardMenu";
+import AddQuestForm from "@/app/components/forms/AddQuestForm";
 import { db } from "@/db";
-import { Quest } from "@/db/schema";
 import Modal from "@/ui/Modal";
-import { QuestMap } from "@/utils/reorder";
+import mapQuests from "@/utils/mapQuests";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+
 export default async function JournalPage({
   params,
 }: {
   params: { village: string };
 }) {
-  const data = await db.query.villages
-    .findFirst({
-      where: (villages, { eq }) => eq(villages.name, params.village),
-      with: {
-        quests: true,
-      },
-    })
-    .then(
-      (res) =>
-        res?.quests.reduce(
-          (acc: QuestMap, task: Quest) => {
-            acc[task.state].push(task);
-            return acc;
-          },
-          { new: [], active: [], resolved: [], closed: [] }
-        ) ?? { new: [], active: [], resolved: [], closed: [] }
-    );
+  const data = await db.query.villages.findFirst({
+    where: (villages, { eq }) => eq(villages.name, params.village),
+    with: {
+      quests: true,
+    },
+  });
 
   return (
     <div className="flex-1 flex flex-col">
-      <Modal
-        asChild
-        trigger={
-          <button className="inline-flex w-fit gap-2 items-center px-4 py-2 rounded bg-mauve3 m-2">
-            Add new Quest <FontAwesomeIcon icon={faPlus} />
-          </button>
-        }
-        title="Add new Quest"
-      >
-        costam
-      </Modal>
+      <NoticeBoardMenu village={data!} />
       <NoticeBoard
         headers={["new", "active", "resolved", "closed"]}
-        tasks={data}
+        quests={data?.quests!}
       />
     </div>
   );
