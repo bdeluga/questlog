cd1bc1c2ac067a35c822ef5c55e1784a43456bdb
Xewol <bartekdeluga0702@gmail.com>
2023-12-15 07:16:44 +0100

    chore: make panels as client

--- app/(authflow)/[village]/page.tsx
+++ app/(authflow)/[village]/page.tsx
@@ -5,19 +5,12 @@ import Realms from "@/app/components/OverviewPanels/Realms";
 import Social from "@/app/components/OverviewPanels/Social";
 import { db } from "@/db";
 import React from "react";
-
+import { auth } from "@/app/auth";
 export default async function Dashboard({
   params,
 }: {
   params: { village: string };
 }) {
-  const data = await db.query.villages.findFirst({
-    where: (villages, { eq }) => eq(villages.name, decodeURI(params.village)),
-    with: {
-      quests: true,
-    },
-  });
-
   return (
     <div className="grid grid-cols-5 grid-rows-5 gap-4 flex-1 p-4">
       <div className="col-span-2 row-span-5 border border-mauve4 bg-mauve2 rounded-2xl p-4">
@@ -30,7 +23,7 @@ export default async function Dashboard({
         <Enchantments />
       </div>
       <div className="col-start-5 row-start-1 border border-mauve4 bg-mauve2 rounded-2xl p-4">
-        <Level village={data!} />
+        <Level village={params.village} />
       </div>
       <div className="row-span-4 col-start-5 row-start-2 border border-mauve4 bg-mauve2 rounded-2xl p-4">
         <Social />
