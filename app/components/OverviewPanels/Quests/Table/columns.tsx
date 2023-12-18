9581d5fb5a3b78cf116c6f8c2df3fa249c701b11
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-18 16:49:41 +0100

    fix: make vercel deploy

--- app/components/OverviewPanels/Quests/Table/columns.tsx
+++ app/components/OverviewPanels/Quests/Table/columns.tsx
@@ -1,80 +1,80 @@
-"use client";
+// "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
+// import { ColumnDef } from "@tanstack/react-table";
 
-import { DataTableColumnHeader } from "./data-table-column-header";
-import { DataTableRowActions } from "./data-table-row-actions";
-import Checkbox from "@/ui/Checkbox";
-import { Quest } from "@/db/schema";
+// // import { DataTableColumnHeader } from "./data-table-column-header";
+// // import { DataTableRowActions } from "./data-table-row-actions";
+// import Checkbox from "@/ui/Checkbox";
+// import { Quest } from "@/db/schema";
 
-export const columns: ColumnDef<Quest>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-[2px]"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-[2px]"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "id",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Task" />
-    ),
-    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "title",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Title" />
-    ),
-    cell: ({ row }) => {
-      return (
-        <div className="flex space-x-2">
-          <span className="max-w-[500px] truncate font-medium">
-            {row.getValue("title")}
-          </span>
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "status",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Status" />
-    ),
-    cell: ({ row }) => {
-      return (
-        <div className="flex w-[100px] items-center">
-          <span>{row.getValue("status")}</span>
-        </div>
-      );
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id));
-    },
-  },
+// export const columns: ColumnDef<Quest>[] = [
+//   {
+//     id: "select",
+//     header: ({ table }) => (
+//       <Checkbox
+//         checked={
+//           table.getIsAllPageRowsSelected() ||
+//           (table.getIsSomePageRowsSelected() && "indeterminate")
+//         }
+//         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+//         aria-label="Select all"
+//         className="translate-y-[2px]"
+//       />
+//     ),
+//     cell: ({ row }) => (
+//       <Checkbox
+//         checked={row.getIsSelected()}
+//         onCheckedChange={(value) => row.toggleSelected(!!value)}
+//         aria-label="Select row"
+//         className="translate-y-[2px]"
+//       />
+//     ),
+//     enableSorting: false,
+//     enableHiding: false,
+//   },
+//   {
+//     accessorKey: "id",
+//     header: ({ column }) => (
+//       <DataTableColumnHeader column={column} title="Task" />
+//     ),
+//     cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
+//     enableSorting: false,
+//     enableHiding: false,
+//   },
+//   {
+//     accessorKey: "title",
+//     header: ({ column }) => (
+//       <DataTableColumnHeader column={column} title="Title" />
+//     ),
+//     cell: ({ row }) => {
+//       return (
+//         <div className="flex space-x-2">
+//           <span className="max-w-[500px] truncate font-medium">
+//             {row.getValue("title")}
+//           </span>
+//         </div>
+//       );
+//     },
+//   },
+//   {
+//     accessorKey: "status",
+//     header: ({ column }) => (
+//       <DataTableColumnHeader column={column} title="Status" />
+//     ),
+//     cell: ({ row }) => {
+//       return (
+//         <div className="flex w-[100px] items-center">
+//           <span>{row.getValue("status")}</span>
+//         </div>
+//       );
+//     },
+//     filterFn: (row, id, value) => {
+//       return value.includes(row.getValue(id));
+//     },
+//   },
 
-  {
-    id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} />,
-  },
-];
+//   {
+//     id: "actions",
+//     cell: ({ row }) => <DataTableRowActions row={row} />,
+//   },
+// ];
