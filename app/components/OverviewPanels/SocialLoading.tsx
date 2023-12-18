5c8476340aa76cd3626664466272daa7d6d0eb62
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-18 10:59:46 +0100

    feat: loading fallback

---
+++ app/components/OverviewPanels/SocialLoading.tsx
@@ -0,0 +1,17 @@
+export default function SocialLoading() {
+  return (
+    <ul className="p-2 space-y-2 ">
+      {[...Array(10)]?.map((_, idx) => (
+        <li key={idx} className="justify-between flex items-center">
+          <button className="p-2 rounded w-full text-left relative  flex justify-between">
+            <div className="flex items-center gap-1">
+              <div className="h-8 w-8  bg-mauve4 rounded-full" />
+              <div className="bg-mauve4 w-40 h-6" />
+            </div>
+            <div className="h-8 w-8  rounded bg-mauve4" />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
