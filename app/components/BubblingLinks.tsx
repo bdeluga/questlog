d68160cbd8ae4512fde0d86a79632c7400972d74
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-18 10:59:20 +0100

    fix: changed bubbled link so swtiching villages won't bug styles

--- app/components/BubblingLinks.tsx
+++ app/components/BubblingLinks.tsx
@@ -23,19 +23,10 @@ export default function BubblingLinks() {
           key={label}
           href={href}
           className={`relative p-2 ${
-            href === pathname ? "" : "hover:bg-mauve4"
+            href === pathname ? "bg-mauve4 font-bold" : "hover:bg-mauve4"
           } rounded text-sm`}
         >
           {label}
-          {href === pathname && (
-            <motion.span
-              layoutId="bubble"
-              initial={false}
-              className="absolute inset-0  bg-mauve6  mix-blend-difference"
-              style={{ borderRadius: "4px" }}
-              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-            />
-          )}
         </Link>
       ))}
     </nav>
