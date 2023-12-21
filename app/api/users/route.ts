64fdecbbf7e42a49f1ca32840f1c9aa040e94b33
bartod <bartosz.deluga@km.kongsberg.com>
2023-12-21 14:49:36 +0100

    feat: users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,20 +1,24 @@
+import { auth } from "@/app/auth";
 import { db } from "@/db";
-import { users } from "@/db/schema";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const user = await auth();
+
+  if (!user)
+    return NextResponse.json({ stauts: 401, message: "User not authorized" });
+
   const { searchParams } = new URL(request.url);
   const search = searchParams.get("search");
 
-  if (search && search.length > 1) {
+  if (search && search.length > 0) {
     const data = await db.query.users.findMany({
-      where: (users, { ilike }) => ilike(users.name, `%${search}%`),
+      where: (users, { ilike, and, ne }) =>
+        and(ilike(users.name, `%${search}%`), ne(users.id, user.user!.id)),
     });
 
     return NextResponse.json({ data });
   }
 
-  const data = await db.query.users.findMany();
-
-  return NextResponse.json({ data });
+  return NextResponse.json({ status: 500 });
 }
