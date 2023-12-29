f390024ee359eae490251eca18d4c38817edfead
Xewol <bartekdeluga0702@gmail.com>
2023-12-29 09:26:09 +0100

    fix: vercel build

--- db/schema.ts
+++ db/schema.ts
@@ -1,44 +1,47 @@
 import {
+  pgTable,
+  unique,
+  pgEnum,
+  text,
+  date,
+  uniqueIndex,
+  foreignKey,
+  integer,
+  primaryKey,
+} from "drizzle-orm/pg-core";
+import {
   InferInsertModel,
   InferSelectModel,
   relations,
   sql,
 } from "drizzle-orm";
-import {
-  text,
-  date,
-  pgEnum,
-  pgTable,
-  uniqueIndex,
-  integer,
-  primaryKey,
-} from "drizzle-orm/pg-core";
 
-const planEnum = pgEnum("plan", ["hobby", "pro"]);
-const stateEnum = pgEnum("state", [
-  "new",
-  "active",
-  "resolved",
-  "closed",
+export const plan = pgEnum("plan", ["pro", "hobby"]);
+export const state = pgEnum("state", [
   "archived",
+  "closed",
+  "resolved",
+  "active",
+  "new",
 ]);
+export const type = pgEnum("type", ["owner", "member"]);
+
 export const users = pgTable("users", {
   id: text("id")
     .default(sql`gen_random_uuid()`)
     .primaryKey(),
   name: text("name"),
-  createdAt: date("created_at").default(sql`now()`),
+  createdAt: date("created_at").defaultNow(),
   image: text("image"),
   email: text("email").unique(),
   provider: text("provider").default("credentials"),
   providerId: text("provider_id"),
-  plan: planEnum("plan"),
-  selectedVillage: text("selected_village"),
+  plan: plan("plan"),
+  userType: type("type").default("owner"),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
   villages: many(villages),
-  quests: many(quests),
 }));
 
 export type User = InferSelectModel<typeof users>;
@@ -52,17 +55,17 @@ export const villages = pgTable(
       .primaryKey(),
     name: text("name").notNull(),
     userId: text("user_id")
-      .references(() => users.id, { onDelete: "cascade" })
-      .notNull(),
-    exp: integer("exp").notNull().default(1),
-    expNeeded: integer("exp_needed").notNull().default(150),
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    exp: integer("exp").default(1).notNull(),
+    expNeeded: integer("exp_needed").default(150).notNull(),
     level: integer("level").default(1).notNull(),
   },
   (table) => {
     return {
       userVillageIdx: uniqueIndex("user_village_idx").on(
-        table.userId,
-        table.name
+        table.name,
+        table.userId
       ),
     };
   }
@@ -83,35 +86,6 @@ export const villagesRelations = relations(villages, ({ one, many }) => ({
 export type Village = InferSelectModel<typeof villages>;
 export type NewVillage = InferInsertModel<typeof villages>;
 
-export const quests = pgTable("quests", {
-  id: text("id")
-    .default(sql`gen_random_uuid()`)
-    .primaryKey(),
-  number: integer("number")
-    .default(sql`nextval('questnum_seq')`)
-    .notNull(),
-  title: text("title").notNull(),
-  description: text("description").notNull(),
-  createdAt: date("created_at").default(sql`now()`),
-  villageId: text("village_id")
-    .references(() => villages.id, { onDelete: "cascade" })
-    .notNull(),
-  mercenaryId: text("user_id").references(() => users.id),
-  difficulty: text("difficulty").notNull(),
-  rewardExp: integer("reward_exp").notNull(),
-  state: stateEnum("state").default("new").notNull(),
-});
-
-export const questsRelations = relations(quests, ({ one }) => ({
-  village: one(villages, {
-    fields: [quests.villageId],
-    references: [villages.id],
-  }),
-  user: one(users),
-}));
-export type Quest = InferSelectModel<typeof quests>;
-export type NewQuest = InferInsertModel<typeof quests>;
-
 export interface NotArchivedQuest extends Quest {
   state: Exclude<Quest["state"], "archived">;
 }
@@ -120,17 +94,20 @@ export const mercenaries = pgTable(
   "mercenaries",
   {
     userId: text("user_id")
-      .references(() => users.id, { onDelete: "cascade" })
-      .notNull(),
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
     villageId: text("village_id")
-      .references(() => villages.id, { onDelete: "cascade" })
-      .notNull(),
+      .notNull()
+      .references(() => villages.id, { onDelete: "cascade" }),
   },
   (table) => ({
     pk: primaryKey({ columns: [table.userId, table.villageId] }),
+    uniqueConstin: uniqueIndex("unique_mercenary").on(
+      table.userId,
+      table.villageId
+    ),
   })
 );
-
 export const mercenariesRelations = relations(mercenaries, ({ one }) => ({
   user: one(users, {
     fields: [mercenaries.userId],
@@ -143,3 +120,32 @@ export const mercenariesRelations = relations(mercenaries, ({ one }) => ({
 }));
 export type Mercenary = InferSelectModel<typeof mercenaries>;
 export type NewMercenary = InferInsertModel<typeof mercenaries>;
+
+export const quests = pgTable("quests", {
+  id: text("id")
+    .default(sql`gen_random_uuid()`)
+    .primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  createdAt: date("created_at").defaultNow(),
+  villageId: text("village_id")
+    .notNull()
+    .references(() => villages.id, { onDelete: "cascade" }),
+  difficulty: text("difficulty").notNull(),
+  rewardExp: integer("reward_exp").notNull(),
+  state: state("state").default("new").notNull(),
+  // sql`CREATE SEQUENCE questnum_seq;`
+  number: integer("number")
+    .default(sql`nextval('questnum_seq')`)
+    .notNull(),
+});
+
+export const questsRelations = relations(quests, ({ one }) => ({
+  village: one(villages, {
+    fields: [quests.villageId],
+    references: [villages.id],
+  }),
+  user: one(users),
+}));
+export type Quest = InferSelectModel<typeof quests>;
+export type NewQuest = InferInsertModel<typeof quests>;
